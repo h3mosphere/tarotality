a65d885400f798d20e5a87febfc2f45b814e1cab
hemosphere <hemosphere@proton.me>
2023-10-17 13:14:40 +1100

    initial

---
+++ src/routes/Tarot.svelte
@@ -0,0 +1,91 @@
+<script>
+	import { spring } from 'svelte/motion';
+    import tarot_meanings from '$lib/data/tarot-meanings/ekelon-tarot-api.json';
+	let state = [];
+
+    /**
+     * @param {string} short_name
+     */
+    function add_card(short_name) {
+
+    }
+</script>
+
+<div class="card-chooser">
+	<button on:click={() => { console.log('asdf'); }} aria-label="Add this card to the ">
+	</button>
+
+    <ul>
+        {#each tarot_meanings.cards as card}
+        <li>{card.name}</li>
+        {/each}
+    </ul>
+
+</div>
+
+<style>
+	.counter {
+		display: flex;
+		border-top: 1px solid rgba(0, 0, 0, 0.1);
+		border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+		margin: 1rem 0;
+	}
+
+	.counter button {
+		width: 2em;
+		padding: 0;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		border: 0;
+		background-color: transparent;
+		touch-action: manipulation;
+		font-size: 2rem;
+	}
+
+	.counter button:hover {
+		background-color: var(--color-bg-1);
+	}
+
+	svg {
+		width: 25%;
+		height: 25%;
+	}
+
+	path {
+		vector-effect: non-scaling-stroke;
+		stroke-width: 2px;
+		stroke: #444;
+	}
+
+	.counter-viewport {
+		width: 8em;
+		height: 4em;
+		overflow: hidden;
+		text-align: center;
+		position: relative;
+	}
+
+	.counter-viewport strong {
+		position: absolute;
+		display: flex;
+		width: 100%;
+		height: 100%;
+		font-weight: 400;
+		color: var(--color-theme-1);
+		font-size: 4rem;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.counter-digits {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+	}
+
+	.hidden {
+		top: -100%;
+		user-select: none;
+	}
+</style>
